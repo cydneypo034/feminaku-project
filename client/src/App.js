cebbc5bbe75ab5bb0f2afd5f43a6e66a764bcd94
Cydney Pollard <sincerelycydney087@gmail.com>
2019-09-18 11:28:01 -0400

    created pages for user on react front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import AllUsers from "./components/allusers.component";
+import CreateUser from "./components/createuser.component";
+import EditUser from "./components/edituser.component";
+
+
+class App extends Component {
+
+  render() {
+  return (
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route path="/" component={AllUsers} />
+          <Route path="/create" component={CreateUser} />
+          <Route path="/edit/:id" component={EditUser} />
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
+  }
+}
+  
+
+export default App;
+
+/*
 const singleUser = ({name, originCity, age, instagram, favoriteAnime, favoriteFood}) => {
   return (
     <li>{name} - {originCity} - {age} - {instagram} - {favoriteAnime} - {favoriteFood} </li>
@@ -28,12 +56,6 @@ function App() {
     favoriteAnime: "Favorite Anime: Attack on Titan",
     favoriteFood: "Favorite Food: Ramen"
   } ]
-  return (
-    <div>
-      <h1>Feminaku</h1>
-      {multipleUsers(testUser)}
-    </div>
-  );
-}
-
-export default App;
+  
+  {multipleUsers(testUser)}
+*/
