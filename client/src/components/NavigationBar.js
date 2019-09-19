c71858136872c0674eb10ed835d799df4884dee0
Cydney Pollard <sincerelycydney087@gmail.com>
2019-09-19 05:59:11 -0400

    added navbar, home and about page with bootstrap

---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Nav, Navbar} from 'react-bootstrap';
+
+
+class NavBar extends React.Component {
+    render() {
+        return (
+           <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">Feminaku</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar> 
+        )
+    }
+
+}
+
+export default NavBar;
