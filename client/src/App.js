08b1063c4cfb3ebfcc6ecf37c3135c11e7b58fe0
Cydney Pollard <sincerelycydney087@gmail.com>
2019-09-18 20:28:50 -0400

    started over on react

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,26 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import AllUsers from "./components/allusers.component";
-import CreateUser from "./components/createuser.component";
-import EditUser from "./components/edituser.component";
-
-
-class App extends Component {
-
-  render() {
-  return (
-    <React.Fragment>
-      <Router>
-        <Switch>
-          <Route path="/" component={AllUsers} />
-          <Route path="/create" component={CreateUser} />
-          <Route path="/edit/:id" component={EditUser} />
-        </Switch>
-      </Router>
-    </React.Fragment>
-  );
-  }
-}
-  
-
-export default App;
-
-/*
-const singleUser = ({name, originCity, age, instagram, favoriteAnime, favoriteFood}) => {
-  return (
-    <li>{name} - {originCity} - {age} - {instagram} - {favoriteAnime} - {favoriteFood} </li>
-  )
-}
-
-const multipleUsers = (userinfo) => {
-  return (
-    <ul>{userinfo.map(singleUser)}</ul>
-  )
-}
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const testUser = [{
-    name: "Name : Lacy Adams",
-    originCity: "Origin City: Norway",
-    age: 19,
-    instagram: "Instagram: @fixitfelix",
-    favoriteAnime: "Favorite Anime: Attack on Titan",
-    favoriteFood: "Favorite Food: Ramen"
-  }, {
-    name: "Name : Mary Adams",
-    originCity: "Origin City: Norway",
-    age: 19,
-    instagram: "Instagram: @fixitfelix",
-    favoriteAnime: "Favorite Anime: Attack on Titan",
-    favoriteFood: "Favorite Food: Ramen"
-  } ]
-  
-  {multipleUsers(testUser)}
-*/
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
