737dc027d9fe689d875cf23f79c3a2f33b868865
Cydney Pollard <sincerelycydney087@gmail.com>
2019-09-23 15:28:13 -0400

    tried to add dropdown nav bar again, will do another time.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,11 +7,8 @@ class About extends React.Component {
             <div className="App2">
                 <h1 className="about-centered">Welcome To Feminaku!</h1>
                 <p className="about-paragraph">Feminaku strives to provide a safe space for women 
-                who love all genres of anime. Our community is sacred space for connecting with and learning
-                from one another. Feel free to connect with other users and let's share our love of anime together.</p>
-
-
-                
+                who love all genres of anime. Feel free to connect with other users and let's 
+                share our love of anime together.</p>
             </div>
         )
     }
