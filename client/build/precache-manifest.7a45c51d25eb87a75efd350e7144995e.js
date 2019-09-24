5c1c29d12d73f2aa76073e7eadd85d7ce213a442
Cydney Pollard <sincerelycydney087@gmail.com>
2019-09-23 22:08:10 -0400

    changed connection.js

---
+++ client/build/precache-manifest.7a45c51d25eb87a75efd350e7144995e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1766519f41c9502bcc10770abd91bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5065e84b6ed725deb8ab",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5fee5a0a7cbf454a4fd4",
+    "url": "/static/css/main.6906017a.chunk.css"
+  },
+  {
+    "revision": "5065e84b6ed725deb8ab",
+    "url": "/static/js/2.fae3c59c.chunk.js"
+  },
+  {
+    "revision": "5fee5a0a7cbf454a4fd4",
+    "url": "/static/js/main.3825a774.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "75cd6d2187d4234227929e445e188f4e",
+    "url": "/static/media/bulmadbz.75cd6d21.gif"
+  },
+  {
+    "revision": "0a08e4462ee74f46caeed679dfdb2e73",
+    "url": "/static/media/cuteanimegirl.0a08e446.png"
+  },
+  {
+    "revision": "46019cd1fad53df880494da1d3674333",
+    "url": "/static/media/dawn-pokemon.46019cd1.gif"
+  },
+  {
+    "revision": "2e9d1ce4ed0102ca322dc52378414ee2",
+    "url": "/static/media/hanamutou.2e9d1ce4.gif"
+  },
+  {
+    "revision": "444629fe320c3d24bcf339cbc27e2d2b",
+    "url": "/static/media/howlandsohpie.444629fe.gif"
+  },
+  {
+    "revision": "6b5f83260213e78e1741bf2022d1c154",
+    "url": "/static/media/kawaiigirl.6b5f8326.jpg"
+  },
+  {
+    "revision": "7a7dcfa6474ec4cbfa81113eebe3c0dc",
+    "url": "/static/media/maxresdefault.7a7dcfa6.jpg"
+  },
+  {
+    "revision": "efd5f314fa880ee2d0008483dbca4596",
+    "url": "/static/media/revolutionutena.efd5f314.jpg"
+  },
+  {
+    "revision": "a2648d6ff31e0bbb30e2346f9801e45f",
+    "url": "/static/media/sailor-moonhearts.a2648d6f.gif"
+  },
+  {
+    "revision": "8001569859887e1891fbe2305f1013e6",
+    "url": "/static/media/sailorusagi.80015698.jpg"
+  }
+]);
--- models/connection.js
+++ models/connection.js
@@ -38,7 +38,7 @@ module.exports = mongoose
 */
 
 // Connect to database
-/*
+
 if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 }
@@ -52,7 +52,6 @@ mongoose.connection.on('error', function(err) {
 );
 mongoose.connection.once('open', function() {
   console.log("Mongoose has connected to MongoDB!");
-});
-*/
+})
 
 module.exports = mongoose
